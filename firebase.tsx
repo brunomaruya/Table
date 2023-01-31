0fb1a09b3a0411807f7af11a287ca1352d2a5051
Bruno <brun0yuj1ym02@gmail.com>
2023-01-31 18:53:08 +0900

    get data from firebase

---
+++ firebase.tsx
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import { useState } from 'react';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAkOBQBRoY1Ml6su6xedMFK1kQLWRvLGLw',
+  authDomain: 'tests-89f25.firebaseapp.com',
+  projectId: 'tests-89f25',
+  storageBucket: 'tests-89f25.appspot.com',
+  messagingSenderId: '260604134151',
+  appId: '1:260604134151:web:5cedfb398839a9e47bbba2',
+  measurementId: 'G-5BP7KTP2QR',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+export const colRef = collection(db, 'Animes');
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,40 @@
 import { Layout } from '@/components/Layout';
+import { colRef } from '@/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+
+interface IAnimes {
+  id: string;
+  Author: string;
+  AnimeName: string;
+}
 
 export default function Home() {
+  const [animes, setAnimes] = useState<IAnimes[]>();
+
+  useEffect(() => {
+    const fetchAnimes = async () => {
+      await getDocs(colRef).then((snapshot) => {
+        const newData: any = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setAnimes(newData);
+      });
+    };
+
+    fetchAnimes();
+  }, []);
+
   return (
     <>
       <Layout>
         <h1>Home</h1>
+        <div>
+          {animes
+            ? animes.map((anime) => <div key={anime.id}>{anime.AnimeName}</div>)
+            : ''}
+        </div>
       </Layout>
     </>
   );
